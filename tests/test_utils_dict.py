5ce6448206f040b1aed23b39a63b486a9ad4657a
Nick Stuer <nickstuer@gmail.com>
2025-07-23 05:38:00 -0400

    change sample_dict

--- tests/test_utils_dict.py
+++ tests/test_utils_dict.py
@@ -1,3 +1,5 @@
+import copy
+
 import pytest
 from beartype.roar import BeartypeHintViolation
 
@@ -10,20 +12,24 @@ from zettings.utils.dict_utils import (
 
 
 # fixtures
-@pytest.fixture
-def sample_dict():
-    return {
-        "key1": "value1",
-        "key2": "value2",
-        "key3": {"subkey1": "subvalue1", "subkey2": "subvalue2", "subkey3": {"subsubkey1": "subsubvalue1"}},
-        "key4": ["listitem1", "listitem2", "listitem3"],
-        "key5": {"subkey1": ["listitem1", "listitem2", "listitem3"], "subkey2": "subvalue2"},
-        "key6": "value6",
-        "key7": {"subkey1": "subvalue1", "subkey2": "subvalue2", "subkey3": {"subsubkey1": "subsubvalue1"}},
-        "key8": True,
-        "key9": 69,
-        "key10": 3.14,
-    }
+@pytest.fixture(
+    params=[
+        {
+            "key1": "value1",
+            "key2": "value2",
+            "key3": {"subkey1": "subvalue1", "subkey2": "subvalue2", "subkey3": {"subsubkey1": "subsubvalue1"}},
+            "key4": ["listitem1", "listitem2", "listitem3"],
+            "key5": {"subkey1": ["listitem1", "listitem2", "listitem3"], "subkey2": "subvalue2"},
+            "key6": "value6",
+            "key7": {"subkey1": "subvalue1", "subkey2": "subvalue2", "subkey3": {"subsubkey1": "subsubvalue1"}},
+            "key8": True,
+            "key9": 69,
+            "key10": 3.14,
+        }
+    ],
+)
+def sample_dict(request):
+    return copy.deepcopy(request.param)
 
 
 @pytest.fixture(
